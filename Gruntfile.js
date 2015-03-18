01f06ca0468d492027a5c67c9aaaf31701996406
Bruce Mitchener <bruce.mitchener@gmail.com>
2015-03-18 18:35:40 +0700

    Add support for mocha-based testing.
    This also splits the TSD files into a bundle for the library and
    another for the tests.

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,12 +12,32 @@ module.exports = function(grunt) {
         }
       }
     },
+    'mochaTest': {
+      test: {
+        options: {
+          reporter: 'spec',
+          quiet: false,
+          clearRequireCache: true
+        },
+        src: ['test/**/*.js']
+      }
+    },
     'tsd': {
-      refresh: {
+      lib: {
         options: {
           command: 'reinstall',
           latest: true,
-          config: 'conf/tsd.json',
+          config: 'conf/tsd-lib.json',
+          opts: {
+            // props from tsd.Options
+          }
+        }
+      },
+      test: {
+        options: {
+          command: 'reinstall',
+          latest: true,
+          config: 'conf/tsd-test.json',
           opts: {
             // props from tsd.Options
           }
@@ -30,29 +50,48 @@ module.exports = function(grunt) {
           configuration: grunt.file.readJSON('conf/tslint.json')
         },
         files: {
-          src: ['src/**/*.ts']
+          src: [
+            'src/**/*.ts',
+            'test/**/*.ts'
+          ]
         }
       }
     },
     'typescript': {
-       base: {
-          src: ['src/**/*.ts'],
-          dest: 'app/src',
-          options: {
-             basePath: 'src',
-             sourceMap: true,
-             target: 'es5'
-          }
+       lib: {
+         src: ['src/**/*.ts'],
+         dest: 'app/src',
+         options: {
+           basePath: 'src',
+           module: 'commonjs',
+           sourceMap: true,
+           target: 'es5'
+         }
+       },
+       test: {
+         src: ['test/**/*.ts'],
+         options: {
+           module: 'commonjs',
+           sourceMap: true,
+           target: 'es5'
+         }
        }
     },
     'watch': {
-      files: '**/*.ts',
-      tasks: ['typescript']
+      lib: {
+        files: 'src/**/*.ts',
+        tasks: ['tslint', 'typescript']
+      },
+      test: {
+        files: 'test/**/*.ts',
+        tasks: ['test']
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-tsd');
   grunt.loadNpmTasks('grunt-tslint');
   grunt.loadNpmTasks('grunt-typescript');
@@ -61,5 +100,9 @@ module.exports = function(grunt) {
 
   grunt.registerTask('build', ['typescript']);
 
-  grunt.registerTask('default', ['lint', 'build']);
+  grunt.registerTask('run-tests', ['mochaTest']);
+
+  grunt.registerTask('test', ['tslint', 'typescript', 'run-tests']);
+
+  grunt.registerTask('default', ['lint', 'build', 'run-tests']);
 };
